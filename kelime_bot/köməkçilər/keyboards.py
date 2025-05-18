8d878fe0cb5bfe1825d86533ff4556c8019eb899
Lerro <makar1212tg@gmail.com>
2025-05-18 18:52:13 +0400

    Update keyboards.py

--- kelime_bot/köməkçilər/keyboards.py
+++ kelime_bot/köməkçilər/keyboards.py
@@ -2,7 +2,7 @@ from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
 
 kanal = InlineKeyboardMarkup([
-    [InlineKeyboardButton("🔮 Əyləncə Qrupu 🔮" , url= "t.me/MedusaSohbet")]
+    [InlineKeyboardButton("🔮 Əyləncə Qrupu 🔮" , url= "t.me/xSohbetGroup")]
 ])
 
 
